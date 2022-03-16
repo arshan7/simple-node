81747ba28af5fe7f45447bba9fb266623ffde862
arshan <arshanreddy27@gmail.com>
2022-03-16 07:56:12 +0530

    added React

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, "../react-routing/build")));
+
+console.log(path.resolve(__dirname), "path");
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
@@ -10,4 +15,8 @@ app.get("/api", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
